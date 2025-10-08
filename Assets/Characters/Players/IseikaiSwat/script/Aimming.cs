474eb685629e347d0e6cf09aaf53c47cee7f33f4
AheadAlot <92690997+AheadAlot@users.noreply.github.com>
2025-10-08 18:33:06 -0400

    fix: Player camera use orbit behavior on mouse Y change
    chore: Crab use sneaky animation on wander

--- Assets/Characters/Players/IseikaiSwat/script/Aimming.cs
+++ Assets/Characters/Players/IseikaiSwat/script/Aimming.cs
@@ -20,13 +20,19 @@ public class Aimming : MonoBehaviour
     {
         xAxis += Input.GetAxisRaw("Mouse X") * mouseSense;
         yAxis -= Input.GetAxisRaw("Mouse Y") * mouseSense;
-        yAxis = Mathf.Clamp(yAxis, -80, 80);
+        yAxis = Mathf.Clamp(yAxis, 10, 80);
     }
 
     private void LateUpdate()
     {
-        cameraFollowPos.localEulerAngles = new Vector3(yAxis, cameraFollowPos.localEulerAngles.y, cameraFollowPos.localEulerAngles.z);
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis, transform.eulerAngles.z);
+        // cameraFollowPos.localEulerAngles = new Vector3(yAxis, cameraFollowPos.localEulerAngles.y, cameraFollowPos.localEulerAngles.z);
+        // transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis, transform.eulerAngles.z);
+        transform.eulerAngles = new Vector3(0f, xAxis, 0f);
+
+        Vector3 offset = new Vector3(0f, 0f, -Vector3.Distance(cameraFollowPos.transform.position, transform.position));
+        Quaternion rotation = Quaternion.Euler(yAxis, xAxis, 0f);
+        cameraFollowPos.position = transform.position + rotation * offset;
+        cameraFollowPos.LookAt(transform.position + Vector3.up * 1.5f);
     }
     
 }
