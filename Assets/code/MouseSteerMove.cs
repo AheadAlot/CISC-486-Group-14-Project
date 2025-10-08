ff39ab6c76fad7c4a68d483193bc44b455c0de43
LXY <LSM@123.com>
2025-10-08 02:11:45 -0400

    Update: fixed the character direction following the mouse

---
+++ Assets/code/MouseSteerMove.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class MouseSteerMove : MonoBehaviour
+{
+    [SerializeField] float mouseSense = 2f;
+    [SerializeField] float minPitch = -70f;
+    [SerializeField] float maxPitch = 70f;
+
+    [SerializeField] Transform cameraFollowPos;
+    CharacterController cc;
+
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float gravity = -20f;
+
+    float pitch;                                     
+    float velY;                                     
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        if (!cameraFollowPos) Debug.LogWarning("[MouseSteerMove] cameraFollowPos ");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mx = Input.GetAxisRaw("Mouse X") * mouseSense;
+        float my = Input.GetAxisRaw("Mouse Y") * mouseSense;
+
+        transform.Rotate(0f, mx, 0f, Space.Self);
+
+        pitch = Mathf.Clamp(pitch - my, minPitch, maxPitch);
+        if (cameraFollowPos)
+            cameraFollowPos.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = (transform.forward * v + transform.right * h);
+        if (moveDir.sqrMagnitude > 1f) moveDir.Normalize();
+
+        if (cc.isGrounded && velY < 0f) velY = -2f;
+        velY += gravity * Time.deltaTime;
+
+        Vector3 motion = moveDir * moveSpeed + Vector3.up * velY;
+        cc.Move(motion * Time.deltaTime);
+    }
+}
